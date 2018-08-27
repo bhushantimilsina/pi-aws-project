1d7665f285b12d2927abf85379e1cd88dd932e55
bhush <bhushantimilsina@gmail.com>
2018-08-26 21:07:48 -0400

    Fixed issue

--- src/main/java/com/aws/codestar/projecttemplates/services/LightController.java
+++ src/main/java/com/aws/codestar/projecttemplates/services/LightController.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Controller;
 
 import com.aws.codestar.projecttemplates.model.ClientInboundMessage;
 import com.aws.codestar.projecttemplates.model.ClientOutboundCommand;
-import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.PinState;
@@ -23,7 +22,7 @@ public class LightController {
 	private static final Logger log = LoggerFactory.getLogger(LightController.class);
 	
 	// This instance needs to be static since it will be associated 
-	private static GpioPinDigitalOutput green;
+	private static GpioPinDigitalOutput green = GpioFactory.getInstance().provisionDigitalOutputPin(RaspiPin.GPIO_01);// GPIO;
 	
 	@Autowired
 	ClientInboundMessage toClientMessage;
@@ -31,11 +30,6 @@ public class LightController {
 	@MessageMapping("/client-outbound-command")
 	@SendTo("/topic/client-inbound-messages")
 	public ClientInboundMessage lightCommand(final ClientOutboundCommand command) {
-		// create GPIO controller
-		final GpioController gpio = GpioFactory.getInstance();
-		
-		// provision Broadcom GPIO pin #02 as an output pin and turn on
-		green = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01);// GPIO
 		
 		// set shutdown state for this pin
 		green.setShutdownOptions(true, PinState.LOW);
