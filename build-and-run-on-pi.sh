3600f00af380a328e8a01f01e63d54443325c04b
bhush <bhushantimilsina@gmail.com>
2018-08-28 23:19:53 -0400

    Build Script

--- build-and-run-on-pi.sh
+++ build-and-run-on-pi.sh
@@ -5,18 +5,19 @@
 echo
 echo -e "\e[97mcd /home/pi/git/pi-aws-project\e[0m"
 cd /home/pi/git/pi-aws-project
-echo -e "\e[90mRunning Maven: mvn clean package install ...\e[0m"
+echo -e "\e[90mRunning Maven: mvn clean package ...\e[0m"
 
 # JVM needs root access
 # sudo -su root must be executed before this script
 
 echo
-/opt/apache-maven-3.2.5/bin/mvn clean package -Dmaven.test.skip=true
-
+mvn clean package -Dmaven.test.skip=true
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
 echo -e "\e[91m*** Removing WAR /opt/apache-tomcat-9.0.11/webapps/*.war"
 echo
 rm /opt/apache-tomcat-9.0.11/webapps/*.war
+
 echo -e "\e[91m*** Removing DIRECTORY /opt/apache-tomcat-9.0.11/webapps/pi-aws-project"
 rm -fr /opt/apache-tomcat-9.0.11/webapps/pi-aws-project
 echo
