7d88e93072adb058db77ae5a502b6d77fa9cfb5e
bhush <bhushantimilsina@gmail.com>
2018-08-31 17:07:30 -0400

    Fixed script issues

--- build-and-run-on-pi.sh
+++ build-and-run-on-pi.sh
@@ -18,17 +18,17 @@ echo -e "\e[90mRunning Maven: mvn clean package ...\e[0m"
 # sudo -su root must be executed before this script
 
 echo
-mvn clean package -Dmaven.test.skip=true
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+/opt/apache-maven-3.2.5/bin/mvn clean package -Dmaven.test.skip=true
+rc=$?; if [ "$result" -ne 0 ]; then exit $rc; fi
 
 echo -e "\e[91m*** Removing WAR /opt/apache-tomcat-9.0.11/webapps/*.war"
 echo
 rm /opt/apache-tomcat-9.0.11/webapps/*.war
-
+ls -
 echo -e "\e[91m*** Removing DIRECTORY /opt/apache-tomcat-9.0.11/webapps/pi-aws-project"
 rm -fr /opt/apache-tomcat-9.0.11/webapps/pi-aws-project
 echo
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+rc=$?; if [ "$result" -ne 0 ]; then exit $rc; fi
 echo
 echo -e "\e[93m*** Copying /home/pi/git/pi-aws-project/target/*.war /opt/apache-tomcat-9.0.11/webapps ...\e[0m"
 cp /home/pi/git/pi-aws-project/target/*.war /opt/apache-tomcat-9.0.11/webapps
@@ -39,4 +39,4 @@ export CLASSPATH="$CLASSPATH:"'/opt/pi4j/lib/*'
 export JAVA_OPTS="$JAVA_OPTS"' -Dpi4j.linking=dynamic'
 
 /opt/apache-tomcat-9.0.11/bin/catalina.sh run
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+rc=$?; if [ "$result" -ne 0 ]; then exit $rc; fi
